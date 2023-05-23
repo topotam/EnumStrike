c6c4650de155f167798df773bb430629f1f63c28
DallasFR <94706450+DallasFR@users.noreply.github.com>
2023-05-23 22:05:36 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1 -O Tools/PowerView.ps1
+wget https://raw.githubusercontent.com/NetSPI/PowerUpSQL/master/PowerUpSQL.ps1 -O Tools/PowerUpSQL.ps1
+wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v3.5%20compiled%20binaries/Rubeus.exe -O Tools/Rubeus.exe
+wget https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.exe -O Tools/SharpHound.exe
+wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v3.5%20compiled%20binaries/Seatbelt.exe -O Tools/Seatbelt.exe
+wget https://raw.githubusercontent.com/itm4n/PrivescCheck/master/PrivescCheck.ps1 -O Tools/PrivescCheck.ps1
+wget https://github.com/akenofu/Sharp-Wifi-Password-Dump/releases/download/0.1/SharpWifiDump.exe -O Tools/SharpWifiDump.exe
+wget https://github.com/djhohnstein/SharpWeb/releases/download/v1.2/SharpWeb.exe -O Tools/SharpWeb.exe
+wget https://raw.githubusercontent.com/Kevin-Robertson/Powermad/master/Powermad.ps1 -O Tools/Powermad.ps1
+wget https://raw.githubusercontent.com/Kevin-Robertson/Inveigh/master/Inveigh.ps1 -O Tools/Inveigh.ps1
+wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/dotnet%20v3.5%20compiled%20binaries/SharpUp.exe -O Tools/SharpUp.exe
